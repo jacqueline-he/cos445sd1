4581c9615282f2f2c82b3e1314d53fd148a20012
Ryan <ryanz@princeton.edu>
2021-02-16 19:20:30 -0500

    test push

--- Student.java
+++ Student.java
@@ -5,22 +5,22 @@
 import java.util.List;
 
 public interface Student {
-  // Given the stats of this simulation, output which schools to apply to
-  // aptitude is drawn from U[0, S]
-  // schools denote the qualities of the colleges drawn from U[0, T] and 
-  // are passed in monotonically decreasing order
-  // synergies are drawn from U[0, W]
-  // schools.length == synergies.length == N >= 10
-  // Return the indicies of the schools to which you want to apply.
-  // Return value `ret` must hold:
-  // * ret.length == 10 and the elements of ret are all different
-  // * forall school in ret, 0 <= school < schools.length
-  public int[] getApplications(
-      int N,
-      double S,
-      double T,
-      double W,
-      double aptitude,
-      List<Double> schools,
-      List<Double> synergies);
+    // Given the stats of this simulation, output which schools to apply to
+    // aptitude is drawn from U[0, S]
+    // schools denote the qualities of the colleges drawn from U[0, T] and
+    // are passed in monotonically decreasing order
+    // synergies are drawn from U[0, W]
+    // schools.length == synergies.length == N >= 10
+    // Return the indicies of the schools to which you want to apply.
+    // Return value `ret` must hold:
+    // * ret.length == 10 and the elements of ret are all different
+    // * forall school in ret, 0 <= school < schools.length
+    public int[] getApplications(
+            int N,
+            double S,
+            double T,
+            double W,
+            double aptitude,
+            List<Double> schools,
+            List<Double> synergies);
 }
